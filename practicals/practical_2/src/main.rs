9c8498ff92a86b094d70f3dfe083c5e901d2acfc
hayley-d <hayleydod@proton.me>
2025-01-04 16:29:32 +0200

    starting

--- practicals/practical_2/src/main.rs
+++ practicals/practical_2/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+use practical_2::question::Question;
+
+#[tokio::main]
+async fn main() {
+    Question::parse_file().await;
 }
--- practicals/practical_2/src/question.rs
+++ practicals/practical_2/src/question.rs
@@ -1,3 +1,5 @@
+use tokio::fs;
+
 pub struct Question {
     question: String,
     options: Vec<String>,
@@ -5,11 +7,32 @@ pub struct Question {
 }
 
 impl Question {
-    pub fn new(questions: String, options: Vec<String>, answers: Vec<usize>) -> Self {
+    pub fn new(question: String, options: Vec<String>, answers: Vec<usize>) -> Self {
         return Question {
             question,
             options,
             answers,
         };
+    }
+
+    pub fn print(&self) -> String {
+        let mut output: String = format!("\033[1;34mQuestion: {}\x1b[0m", self.question);
+        for (i, option) in self.options.iter().enumerate() {
+            output.push_str(format!("({}) {}", i, option).as_str());
+        }
+
+        return output;
+    }
+
+    pub async fn parse_file() -> Vec<Question> {
+        let file = match fs::read_to_string("file.txt").await {
+            Ok(f) => f,
+            Err(_) => {
+                eprintln!("Error reading file");
+                std::process::exit(1);
+            }
+        };
+        println!("{}", file);
+        return Vec::new();
     }
 }
