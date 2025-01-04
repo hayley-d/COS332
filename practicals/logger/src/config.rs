9778999eed4288aabb55ca6bf980e81e05e437ee
hayley-d <hayleydod@proton.me>
2025-01-04 15:33:13 +0200

    adding own logger implementation

--- practicals/logger/src/config.rs
+++ practicals/logger/src/config.rs
@@ -1,1 +1,13 @@
-pub struct Config {}
+pub struct Config {
+    pub error_file: String,
+    pub log_file: String,
+}
+
+impl Config {
+    pub fn new(error_file: String, log_file: String) -> Self {
+        return Config {
+            error_file,
+            log_file,
+        };
+    }
+}
--- practicals/logger/src/logger.rs
+++ practicals/logger/src/logger.rs
@@ -1,22 +1,85 @@
 use std::fmt::Display;
+use std::sync::Arc;
 
+use chrono::Local;
+use log::Log;
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{AsyncWriteExt, BufWriter};
 use tokio::sync::Mutex;
 
 use crate::config::Config;
 
 pub struct Logger {
-    level: Level,
     config: Config,
-    output_lock: Mutex<()>,
+    error_file: Arc<Mutex<BufWriter<File>>>,
+    log_file: Arc<Mutex<BufWriter<File>>>,
 }
 
 impl Logger {
-    pub fn new(level: Level, config: Config) -> Box<Logger> {
-        return Box::new(Logger {
-            level,
+    /// Create a new logger that can be independently used.
+    ///
+    pub async fn new(config: Config) -> Logger {
+        let error_file = Arc::new(Mutex::new(BufWriter::new(
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&config.error_file)
+                .await
+                .expect("Failed to open error log file"),
+        )));
+
+        let log_file = Arc::new(Mutex::new(BufWriter::new(
+            OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(&config.log_file)
+                .await
+                .expect("Failed to open log log file"),
+        )));
+
+        return Logger {
             config,
-            output_lock: Mutex::new(()),
-        });
+            error_file,
+            log_file,
+        };
+    }
+
+    pub async fn log_error(&self, message: &str, line: u32) {
+        let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string();
+        let log_message = format!(
+            "{} [ERROR] {}:{} - {}\n",
+            timestamp, self.config.error_file, line, message
+        );
+
+        let mut error_file = self.error_file.lock().await;
+        match error_file.write_all(log_message.as_bytes()).await {
+            Ok(_) => (),
+            Err(e) => eprintln!("Error writing to error log: {:?}", e),
+        };
+
+        match error_file.flush().await {
+            Ok(_) => (),
+            Err(e) => eprintln!("Error writing to error log: {:?}", e),
+        };
+    }
+
+    pub async fn log_info(&self, message: &str, line: u32) {
+        let timestamp = Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string();
+        let log_message = format!(
+            "{} [INFO] {}:{} - {}\n",
+            timestamp, self.config.log_file, line, message
+        );
+
+        let mut log_file = self.log_file.lock().await;
+        match log_file.write_all(log_message.as_bytes()).await {
+            Ok(_) => (),
+            Err(e) => eprintln!("Error writing to info log: {:?}", e),
+        };
+
+        match log_file.flush().await {
+            Ok(_) => (),
+            Err(e) => eprintln!("Error writing to info log: {:?}", e),
+        };
     }
 }
 
@@ -57,4 +120,33 @@ impl PartialOrd for Level {
             },
         }
     }
+}
+
+pub trait SharedLogger: Log {
+    /// Returns the set level for the logger.
+    ///
+    /// # Example
+    /// ```
+    /// use logger::*;
+    /// fn main() {
+    ///     let logger = Logger::new(Level::Info,Config::default());
+    ///     println!("{}",logger.level());
+    /// }
+    /// ```
+    fn level(&self) -> Level;
+
+    /// Inspect the config for a logger.
+    ///
+    /// # Example
+    /// ```
+    /// use logger::*;
+    /// fn main() {
+    ///     let logger = Logger::new(Level::Info,Config::default());
+    ///     println!("{:?}",logger.config());
+    /// }
+    /// ```
+    fn config(&self) -> Option<&Config>;
+
+    /// Returns the logger as a Log trait object
+    fn as_log(self: Box<Self>) -> Box<dyn Log>;
 }
