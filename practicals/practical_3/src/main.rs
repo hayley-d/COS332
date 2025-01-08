2e12239e95fa3f0af63de5ba9edbc6d0367a6b5d
hayley-d <hayleydod@proton.me>
2025-01-08 16:02:46 +0200

    fixed logging

--- practicals/practical_3/src/main.rs
+++ practicals/practical_3/src/main.rs
@@ -1,14 +1,13 @@
 use colored::Colorize;
-use log::error;
+use log::{error, info};
 use practical_3::socket::connection::start_server;
-use practical_3::{ErrorType, Logger};
 use std::env;
 
 const DEFAULT_PORT: u16 = 7878;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let logger: Logger = Logger::new("server.log");
+    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
     let port: u16 = match env::args()
         .nth(1)
@@ -16,112 +15,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .parse()
     {
         Ok(p) => p,
-        Err(_) => {
-            error!("Failed to parse port number");
-            let error = ErrorType::SocketError(String::from("Problem parsing port"));
-            logger.log_error(&error);
-            DEFAULT_PORT
-        }
+        Err(_) => DEFAULT_PORT,
     };
 
     print_server_info(port);
 
+    info!(target: "request_logger","Server Started");
     let _ = start_server(port).await;
 
     Ok(())
 }
-
-/*async fn run_server(mut listener: Listener, clock: Arc<Mutex<Clock>>) -> Result<(), ErrorType> {
-    loop {
-        let c = Arc::clone(&clock);
-
-        // Returns an error when the semaphore has been closed, since I do not close it
-        // unwrap should be safe
-        let permit = listener
-            .connection_limit
-            .clone()
-            .acquire_owned()
-            .await
-            .unwrap();
-
-        let (client, addr): (TcpStream, SocketAddr) = match listener.accept().await {
-            Ok((c, a)) => (c, a.into()),
-            Err(_) => {
-                error!("Failed to connect to the client");
-                return Err(ErrorType::SocketError(String::from(
-                    "Error connecting to client",
-                )));
-            }
-        };
-
-        let mut handler = ConnectionHandler {
-            stream: client,
-            addr,
-            shutdown_rx: listener.shutdown_tx.lock().await.subscribe(),
-        };
-
-        tokio::spawn(async move {
-            loop {
-                let lam = Arc::clone(&c);
-
-                let mut buffer: [u8; 4096] = [0; 4096];
-                let bytes_read =
-                    match timeout(Duration::from_secs(5), handler.stream.read(&mut buffer)).await {
-                        Ok(Ok(number_bytes)) if number_bytes == 0 => break,
-                        Ok(Ok(number_bytes)) => number_bytes,
-                        Ok(Err(_)) => {
-                            error!("Failed to connect to client");
-                            break;
-                        }
-                        Err(_) => break,
-                    };
-
-                // check request for any potential maliciousness
-                match handle_request(&buffer[..bytes_read]) {
-                    Ok(_) => (),
-                    Err(_) => {
-                        error!("Request failed test for possible malicious threats");
-                    }
-                };
-
-                let request_id: i64 = lam.lock().await.increment_time();
-
-                let request: Request =
-                    match Request::new(&buffer[..bytes_read], addr.ip(), request_id) {
-                        Ok(r) => {
-                            r.print();
-                            r
-                        }
-                        Err(_) => {
-                            error!("Failed to create request");
-                            break;
-                        }
-                    };
-
-                let mut response = handle_response(request).await;
-
-                if let Err(_) = handler.stream.write_all(&response.to_bytes()).await {
-                    error!("Failed to connect to client");
-                }
-
-                if !handler.shutdown_rx.is_empty() {
-                    let msg: Message = match handler.shutdown_rx.recv().await {
-                        Ok(m) => m,
-                        Err(_) => {
-                            error!("Failed to receive message from shutdown sender");
-                            Message::ServerRunning
-                        }
-                    };
-
-                    if msg == Message::Terminate {
-                        break;
-                    }
-                }
-            }
-            drop(permit);
-        });
-    }
-}*/
 
 fn print_server_info(port: u16) {
     println!("{}", "Server started:".cyan());
