0d6c1e386db52524a2063a2aae651bace2e85630
hayley-d <hayleydod@proton.me>
2025-01-13 09:55:42 +0200

    consistent hashing change

--- practicals/load_balancer/src/main.rs
+++ practicals/load_balancer/src/main.rs
@@ -1,16 +1,15 @@
 use bytes::Bytes;
+use dotenv::*;
 use http_body_util::Full;
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
-use hyper_util::rt::TokioIo;
+use hyper::Response;
 use load_balancer::load_balancer::load_balancer::LoadBalancer;
 use load_balancer::request::buffer_to_request;
+use std::env;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::Mutex;
-use tokio_util::codec::Framed;
+use tokio::sync::{Mutex, Notify};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -29,92 +28,87 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Listening on http://{}", addr);
 
-    let state: LoadBalancer = match LoadBalancer::new(2, &mut nodes).await {
-        Ok(s) => s,
-        Err(_) => std::process::exit(1),
-    };
+    let state: Arc<Mutex<LoadBalancer>> = Arc::new(Mutex::new(LoadBalancer::new(&mut nodes).await));
 
-    let state: Arc<Mutex<LoadBalancer>> = Arc::new(Mutex::new(state));
-
-    let graceful = hyper_util::server::graceful::GracefulShutdown::new();
+    let graceful = Arc::new(Notify::new());
+    let notify_clone = graceful.clone();
     let mut signal = std::pin::pin!(shutdown_signal());
+    tokio::spawn(async move {
+        shutdown_signal().await;
+        notify_clone.notify_waiters();
+    });
 
-    loop {
-        let state = state.clone();
-        tokio::select! {
-            _ = reverse_proxy(listener,state.clone()) => {
-            },
-            _ = &mut signal => {
-                eprintln!("graceful shutdown signal recieved");
-                break;
-            }
-        }
-    }
-
-    // Now start the shutdown and wait for them to complete
     tokio::select! {
-        _ = graceful.shutdown() => {
-            eprintln!("all connections gracefully closed");
+        _ = reverse_proxy(listener,state.clone()) => {
+            println!("loop ended");
         },
-        _ = tokio::time::sleep(std::time::Duration::from_secs(5)) => {
-            eprintln!("timed out wait for all connections to close");
+        _ = &mut signal => {
+            eprintln!("graceful shutdown signal recieved");
         }
+        _ = graceful.notified() => {
+                eprintln!("Graceful shutdown initiated");
+                std::process::exit(0);
+            }
     }
 
     Ok(())
 }
 
-async fn reverse_proxy(
-    listener: TcpListener,
-    state: Arc<Mutex<LoadBalancer>>,
-) -> Result<(), Box<dyn std::error::Error>> {
+async fn reverse_proxy(listener: TcpListener, state: Arc<Mutex<LoadBalancer>>) {
     loop {
-        let (stream, client_address) = listener.accept().await;
+        let state = state.clone();
+        if let Ok((mut stream, client_address)) = listener.accept().await {
+            tokio::spawn(async move {
+                let mut buffer: [u8; 4096] = [0; 4096];
 
-        tokio::spawn(async move {
-            let mut buffer: [u8; 4096] = [0; u8];
+                while let Ok(bytes_read) = stream.read(&mut buffer).await {
+                    if bytes_read == 0 {
+                        return;
+                    }
 
-            let bytes_read = match stream.read(&mut buffer).await {
-                Ok(0) => return,
-                Ok(n) => n,
-                Err(_) => return,
-            };
+                    let mut request: http::Request<Vec<u8>> =
+                        match buffer_to_request(buffer[..bytes_read].to_vec()) {
+                            Ok(request) => request,
+                            Err(e) => {
+                                eprintln!("Failed to parse request: {}", e);
+                                send_error_response(400, &mut stream).await;
+                                return;
+                            }
+                        };
 
-            let request: http::Request<Vec<u8>> = match buffer_to_request(buffer) {
-                Ok(request) => request,
-                _ => return,
-            };
+                    // Ignore favicon.ico requests
+                    if request.uri().path() == "/favicon.ico" {
+                        return;
+                    }
 
-            // Ignore favicon.ico requests
-            if request.uri().path() == "/favicon.ico" {
+                    // add the client IP address custom header
+                    request
+                        .headers_mut()
+                        .insert("X-Client-IP", client_address.to_string().parse().unwrap());
+
+                    let uri = request.uri().path().to_string();
+
+                    let request: load_balancer::request::Request =
+                        load_balancer::request::Request::new(
+                            uri,
+                            client_address.to_string(),
+                            request,
+                        );
+
+                    if state.lock().await.insert(request).await {
+                        // request got added
+                        let _ = state.lock().await.distribute().await;
+                        return;
+                    } else {
+                        // request not added respond status 429 too many requests
+                        send_error_response(429, &mut stream).await;
+                        return;
+                    }
+                }
                 return;
-            }
-
-            // add the client IP address custom header
-            request
-                .headers_mut()
-                .insert("X-Client-IP", client_address.to_string().parse().unwrap());
-
-            let uri = request.uri().path().to_string();
-
-            let request: load_balancer::request::Request =
-                load_balancer::request::Request::new(uri, client_address.to_string(), req);
-
-            if state.lock().await.insert(request).await {
-                // request got added
-                let _ = state.lock().await.distribute().await;
-            } else {
-                // request not added respond status 429 too many requests
-
-                return Ok(Response::builder()
-                    .status(429)
-                    .body(Full::new(Bytes::from("Too Many Request")))
-                    .unwrap());
-            }
-        });
+            });
+        }
     }
-
-    todo!()
 }
 
 async fn shutdown_signal() {
@@ -122,6 +116,7 @@ async fn shutdown_signal() {
     tokio::signal::ctrl_c()
         .await
         .expect("failed to install CTRL+C signal handler");
+    eprintln!("Shutdown signal received...");
 }
 
 fn get_nodes() -> Vec<String> {
@@ -139,4 +134,22 @@ fn get_nodes() -> Vec<String> {
     println!("Loaded nodes");
 
     nodes
+}
+
+async fn send_error_response(code: u64, stream: &mut TcpStream) {
+    match code {
+        429 => {
+            let response_bytes =
+                format!("HTTP/1.1 429 Too Many Requests\r\nContent-Length: 0\r\n\r\n",)
+                    .into_bytes();
+
+            let _ = stream.write_all(&response_bytes).await;
+        }
+        _ => {
+            let response_bytes =
+                format!("HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n",).into_bytes();
+
+            let _ = stream.write_all(&response_bytes).await;
+        }
+    }
 }
