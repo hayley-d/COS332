e2fe92002624d459752708e6bcc35572f6935e99
hayley-d <hayleydod@proton.me>
2025-01-14 08:39:24 +0200

    docuemtation

--- practicals/practical_1/src/api.rs
+++ practicals/practical_1/src/api.rs
@@ -1,3 +1,10 @@
+/// This module provides the core functionality for handling HTTP requests and
+/// generating appropriate responses in an asynchronous server.
+///
+/// It includes:
+/// - Utility functions for reading files.
+/// - Handlers for various HTTP methods (GET, POST, PUT, PATCH, DELETE).
+/// - Integration with shared state for caching and user management.
 use crate::response::{MyDefault, Response};
 use crate::server::SharedState;
 use crate::{ContentType, HttpCode, HttpMethod, Request};
@@ -12,6 +19,16 @@ use tokio::process::Command;
 use tokio::sync::Mutex;
 use uuid::Uuid;
 
+/// Reads the content of a file located at `path` and returns it as a `Vec<u8>`.
+///
+/// # Arguments
+/// - `path`: A string slice that holds the file path.
+///
+/// # Returns
+/// A vector of bytes representing the file content.
+///
+/// # Panics
+/// Panics if the file cannot be opened or read.
 pub async fn read_file_to_bytes(path: &str) -> Vec<u8> {
     let metadata = fs::metadata(path).await.unwrap();
     let mut file = File::open(path).await.unwrap();
@@ -20,6 +37,15 @@ pub async fn read_file_to_bytes(path: &str) -> Vec<u8> {
     buffer
 }
 
+/// Fetches the byte content of a file from the cache or reads and caches it if not present.
+///
+/// # Arguments
+/// - `state`: A shared, thread-safe state used for caching.
+/// - `file_path`: The path to the file to read.
+/// - `route_name`: The name of the route associated with the file.
+///
+/// # Returns
+/// A vector of bytes representing the file content.
 async fn get_bytes(
     state: Arc<Mutex<SharedState>>,
     file_path: PathBuf,
@@ -37,7 +63,14 @@ async fn get_bytes(
     };
 }
 
-/// Entry point into the REST API
+/// Handles incoming HTTP requests and routes them to the appropriate method-specific handler.
+///
+/// # Arguments
+/// - `request`: The incoming HTTP request.
+/// - `state`: A shared, thread-safe state used for managing server data and caching.
+///
+/// # Returns
+/// A `Response` object generated based on the request.
 pub async fn handle_response(request: Request, state: Arc<Mutex<SharedState>>) -> Response {
     match request.method {
         HttpMethod::GET => handle_get(request, state).await,
@@ -48,6 +81,14 @@ pub async fn handle_response(request: Request, state: Arc<Mutex<SharedState>>) -
     }
 }
 
+/// Handles HTTP GET requests, serving static files and handling special routes.
+///
+/// # Arguments
+/// - `request`: The incoming GET request.
+/// - `state`: A shared, thread-safe state used for managing server data and caching.
+///
+/// # Returns
+/// A `Response` object with the appropriate content and status code.
 async fn handle_get(request: Request, state: Arc<Mutex<SharedState>>) -> Response {
     let mut response = Response::default()
         .await
@@ -112,6 +153,14 @@ async fn handle_get(request: Request, state: Arc<Mutex<SharedState>>) -> Respons
     response
 }
 
+/// Handles HTTP POST requests for specific routes like `/signup` and `/login`.
+///
+/// # Arguments
+/// - `request`: The incoming POST request.
+/// - `state`: A shared, thread-safe state used for managing server data and caching.
+///
+/// # Returns
+/// A `Response` object with the appropriate content and status code.
 async fn handle_post(request: Request, state: Arc<Mutex<SharedState>>) -> Response {
     let mut response = Response::default()
         .await
@@ -233,8 +282,16 @@ async fn handle_post(request: Request, state: Arc<Mutex<SharedState>>) -> Respon
         .code(HttpCode::BadRequest)
 }
 
+/// Handles HTTP PUT requests which are currently unsupported and return a `405` Method Not
+/// Allowed.
+///
+/// # Arguments
+/// - `request`: The incoming POST request.
+///
+/// # Returns
+/// A `Response` object with the appropriate content and status code.
 async fn handle_put(request: Request) -> Response {
-    info!("{}", request);
+    info!(target: "request_logger","PUT {} status 405", request.uri);
 
     Response::default()
         .await
@@ -243,92 +300,39 @@ async fn handle_put(request: Request) -> Response {
         .code(HttpCode::MethodNotAllowed)
 }
 
+/// Handles HTTP PATCH requests which are currently unsupported and return a `405` Method Not
+/// Allowed.
+///
+/// # Arguments
+/// - `request`: The incoming POST request.
+///
+/// # Returns
+/// A `Response` object with the appropriate content and status code.
 async fn handle_patch(request: Request) -> Response {
-    info!("PATCH {} status 404", request.uri);
+    info!(target: "request_logger","PATCH {} status 405", request.uri);
     Response::default()
         .await
         .compression(request.is_compression_supported())
         .body(read_file_to_bytes("static/index.html").await)
         .code(HttpCode::MethodNotAllowed)
 }
+
+/// Handles HTTP DELETE requests which are currently unsupported and return a `405` Method Not
+/// Allowed.
+///
+/// # Arguments
+/// - `request`: The incoming POST request.
+///
+/// # Returns
+/// A `Response` object with the appropriate content and status code.
 
 async fn handle_delete(request: Request, state: Arc<Mutex<SharedState>>) -> Response {
-    let response = Response::default()
+    info!(target: "request_logger","Delete {} status 405", request.uri);
+    Response::default()
         .await
         .compression(request.is_compression_supported())
         .body(read_file_to_bytes("static/index.html").await)
-        .code(HttpCode::BadRequest)
-        .content_type(ContentType::Text);
-
-    let username: HashMap<String, String> = match serde_json::from_str(&request.body) {
-        Ok(u) => u,
-        Err(_) => {
-            error!("Failed to parse invalid JSON");
-            return response
-                .body(String::from("Invalid JSON").into())
-                .code(HttpCode::BadRequest);
-        }
-    };
-
-    let username: &String = &username["username"];
-
-    let cookie_header: Vec<String> = request
-        .headers
-        .into_iter()
-        .filter(|h| h.contains("Cookie: session="))
-        .collect();
-
-    let cookie_header = match cookie_header.first() {
-        Some(h) => h,
-        None => {
-            error!("Attempt to delete without proper authentification from IP address");
-            return response
-                .body(String::from("Unable to delete file without proper authentification.").into())
-                .code(HttpCode::BadRequest);
-        }
-    };
-
-    let header_parts: Vec<&str> = cookie_header.split_whitespace().collect();
-
-    let cookie_value: &str = match header_parts.get(1) {
-        Some(v) => v,
-        None => {
-            error!("Attempt to delete without proper authentification from IP address");
-            return response
-                .body(String::from("Unable to delete file without proper authentification.").into())
-                .code(HttpCode::BadRequest);
-        }
-    };
-
-    let session_id: Uuid = match state.lock().await.find_user(username.to_string()).await {
-        Ok(s) => s,
-        Err(_) => {
-            error!(target:"error_logger","Failed to find the user");
-            println!(
-                "{} {} {} {}",
-                ">>".red().bold(),
-                "User not found for".red(),
-                request.method.to_string().magenta(),
-                request.uri.cyan()
-            );
-            return response
-                .body(String::from("No user exists with the provided details.").into())
-                .code(HttpCode::BadRequest)
-                .content_type(ContentType::Text);
-        }
-    };
-
-    // cookie_value = session=sessionID
-    if session_id.to_string() == cookie_value {
-        // session has been verified process the delete
-        return response
-            .body(String::from("File successfully deleted.").into())
-            .code(HttpCode::Ok);
-    }
-
-    response
-        .body(String::from("Unable to delete file.").into())
-        .code(HttpCode::BadRequest)
+        .code(HttpCode::MethodNotAllowed)
 }
 
 #[cfg(test)]
