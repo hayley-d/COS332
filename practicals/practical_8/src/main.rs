9e3cdd8b4229c48456f662112ea0a3342e8121f8
hayley-d <hayleydod@proton.me>
2025-02-04 16:56:37 +0200

    set up initial code

--- practicals/practical_8/src/main.rs
+++ practicals/practical_8/src/main.rs
@@ -1,3 +1,23 @@
+use notify::{recommended_watcher, Event, RecursiveMode, Result, Watcher};
+use std::time::Duration;
+use tokio::sync::mpsc::channel;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        eprintln!("No file path provided, please specify file path in command line arguments and try again");
+        std::process::exit(1);
+    }
+    let file_path = args.get(1).unwrap();
+    let (tx, rx) = channel(10);
+    let mut watcher = notify::recommended_watcher(tx)?;
+    watcher.watch(file_path, RecursiveMode::NonRecursive)?;
+
+    loop {
+        match rx.recv() {
+            Ok(event) => println!("File modified: {:?}", event),
+            Err(e) => eprintln!("Watch Error: {:?}", e),
+            _ => (),
+        }
+    }
 }
