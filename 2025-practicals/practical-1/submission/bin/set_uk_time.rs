c42ee65bfa5afc599dc709a9f9314c321d5aca4c
hayley-d <hayleydod@proton.me>
2025-02-17 09:17:53 +0200

    submit p1

---
+++ 2025-practicals/practical-1/submission/bin/set_uk_time.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    timezone: i32,
+    country: String,
+    city: String,
+}
+
+fn write_config(config: &Config) {
+    let yaml = serde_yaml::to_string(config).expect("Failed to serialize YAML");
+    fs::write("static/backend.yaml", yaml).expect("Failed to write file");
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let new_config: Config = Config {
+        timezone: 0,
+        country: "United Kingdom".to_string(),
+        city: "London".to_string(),
+    };
+
+    write_config(&new_config);
+
+    println!("<!DOCTYPE html>");
+    println!("<html lang=\"en\">");
+    println!("<head>");
+    println!("<meta http-equiv='refresh' content='0;url=show_time.cgi'>");
+    println!("</head>");
+    println!("<body>");
+    println!("<h3>Switching to United Kingdom time</h3>");
+    println!("</body>");
+    println!("</html>");
+    Ok(())
+}
---
+++ 2025-practicals/practical-1/submission/bin/show_time.rs
@@ -0,0 +1,45 @@
+use chrono::{FixedOffset, Utc};
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Config {
+    timezone: i32,
+    country: String,
+    city: String,
+}
+
+fn read_config() -> Config {
+    let content = fs::read_to_string("static/backend.yaml").expect("Failed to read backend.yaml");
+    serde_yaml::from_str(&content).expect("Failed to parse YAML")
+}
+
+fn get_time(offset: i32) -> String {
+    let utc_now = Utc::now();
+    let tz_offset = FixedOffset::east_opt(offset * 3600).unwrap();
+    utc_now
+        .with_timezone(&tz_offset)
+        .format("%Y-%m-%d %H:%M:%S")
+        .to_string()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config: Config = read_config();
+    let current_time = get_time(config.timezone);
+    println!("<!DOCTYPE html>");
+    println!("<html lang=\"en\">");
+    println!("<head>");
+    println!("<title>Current time</title>");
+    println!("</head>");
+    println!("<body>");
+    println!(
+        "<h1>The Current Time in {}, {} is {}</h1></body></html>",
+        config.city, config.country, current_time
+    );
+    println!("<a href='set_sa_time.cgi'>Switch South African Time</a></br>");
+    println!("<a href='set_uk_time.cgi'>Switch United Kingdom Time</a>");
+    println!("</body>");
+    println!("</html>");
+
+    Ok(())
+}
