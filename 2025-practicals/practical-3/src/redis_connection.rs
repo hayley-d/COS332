dd3dc6b538e0368b774835d4b56f632b575beb74
hayley-d <hayleydod@proton.me>
2025-02-17 03:47:28 +0200

    converted to pattern matching

---
+++ 2025-practicals/practical-3/src/redis_connection.rs
@@ -0,0 +1,67 @@
+use std::path::Path;
+
+use log::{error, info};
+use redis::Commands;
+use tokio::fs;
+use tokio::process::{Child, Command};
+
+pub async fn start_redis_server() -> Child {
+    match Command::new("redis-server").spawn() {
+        Ok(child) => {
+            println!("Redis server started");
+            info!("Redis server started");
+            child
+        }
+        _ => {
+            eprintln!("Problem starting redis server");
+            error!(target:"error_logger","Failed to start Redis server");
+            std::process::exit(1);
+        }
+    }
+}
+
+pub async fn stop_redis_server(mut redis_child: Child) -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(e) = redis_child.kill().await {
+        eprintln!("Failed to stop redis server");
+        return Err(Box::new(e));
+    }
+
+    println!("Redis server stopped");
+    info!("Redis server stopped");
+
+    Ok(())
+}
+
+/// Returns a connection to the redis instance
+pub fn set_up_redis() -> Result<redis::Connection, Box<dyn std::error::Error>> {
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    Ok(client.get_connection()?)
+}
+
+// caches a files content and returns the string
+pub async fn read_and_cache_page(
+    redis_connection: &mut redis::Connection,
+    path: &Path,
+    route_name: &str,
+) -> Vec<u8> {
+    let content: String = match fs::read_to_string(path.to_path_buf()).await {
+        Ok(content) => content,
+        Err(_) => fs::read_to_string("static/404.html")
+            .await
+            .expect("404 Not Found"),
+    };
+
+    // set for 10 minuets
+    let _: () = redis_connection.set_ex(route_name, &content, 600).unwrap();
+    content.as_bytes().to_vec()
+}
+
+pub async fn get_cached_content(
+    redis_connection: &mut redis::Connection,
+    route_name: &str,
+) -> Option<Vec<u8>> {
+    match redis_connection.get::<_, String>(route_name) {
+        Ok(content) => Some(content.as_bytes().to_vec()),
+        Err(_) => None,
+    }
+}
---
+++ 2025-practicals/practical-3/src/request.rs
@@ -0,0 +1,287 @@
+use crate::ErrorType;
+use colored::Colorize;
+use core::str;
+use log::error;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Clock {
+    lamport_timestamp: i64,
+}
+
+impl Clock {
+    pub fn new() -> Self {
+        Clock {
+            lamport_timestamp: 0,
+        }
+    }
+    pub fn increment_time(&mut self) -> i64 {
+        let temp: i64 = self.lamport_timestamp;
+        self.lamport_timestamp += 1;
+        temp
+    }
+}
+
+impl Default for Clock {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug)]
+pub enum Protocol {
+    Http,
+}
+
+impl Display for Protocol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Protocol::Http => write!(f, "HTTP/1.1"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Header {
+    pub title: String,
+    pub value: String,
+}
+
+impl Display for Header {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} : {}", self.title, self.value)
+    }
+}
+
+#[derive(Debug)]
+pub enum ContentType {
+    Text,
+    Html,
+    Json,
+}
+
+impl Display for ContentType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ContentType::Text => write!(f, "text/plain"),
+            ContentType::Html => write!(f, "text/html"),
+            ContentType::Json => write!(f, "application/json"),
+        }
+    }
+}
+
+pub struct Request {
+    pub request_id: i64,
+    pub client_ip: String,
+    pub headers: Vec<String>,
+    pub body: String,
+    pub method: HttpMethod,
+    pub uri: String,
+}
+
+impl Display for Request {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Request: {{method: {}, path: {}, request_id: {},client_ip: {}}}",
+            self.method, self.uri, self.request_id, self.client_ip
+        )
+    }
+}
+
+impl Request {
+    pub fn print(&self) {
+        println!("{} New Request:", ">>".red().bold());
+        println!("{}{}", self.method.to_string().magenta(), self.uri.cyan());
+    }
+
+    pub fn new(buffer: &[u8], client_ip: String, request_id: i64) -> Result<Request, ErrorType> {
+        // unwrap is safe as request has been parsed for any issues before this is called
+        let request = String::from_utf8(buffer.to_vec()).unwrap();
+
+        // split the request by line
+        let request: Vec<&str> = request.lines().collect();
+
+        if request.len() < 3 {
+            error!(target: "error_logger","Recieved invalid request");
+            return Err(ErrorType::ConnectionError(String::from("Invalid request")));
+        }
+
+        // get the http method from the first line
+        let method: HttpMethod =
+            HttpMethod::new(request[0].split_whitespace().collect::<Vec<&str>>()[0]);
+
+        // get the uri from the first line
+        let mut uri: String = request[0].split_whitespace().collect::<Vec<&str>>()[1].to_string();
+        if uri == "/favicon.ico" {
+            uri = "/".to_string();
+        }
+
+        // headers are the rest of the
+        let mut headers: Vec<String> = Vec::with_capacity(request.len() - 1);
+        let mut body: String = String::new();
+        let mut flag = false;
+        for line in &request[1..] {
+            if line.is_empty() {
+                flag = true;
+                continue;
+            }
+            if flag {
+                body.push_str(line);
+            } else {
+                headers.push(line.to_string());
+            }
+        }
+
+        Ok(Request {
+            request_id,
+            client_ip,
+            headers,
+            body,
+            method,
+            uri,
+        })
+    }
+
+    pub fn is_compression_supported(&self) -> bool {
+        for header in &self.headers {
+            let header = header.to_lowercase();
+
+            if header.contains("firefox") {
+                return false;
+            }
+
+            if header.contains("accept-encoding") {
+                if header.contains(',') {
+                    // multiple compression types
+                    let mut encodings: Vec<&str> =
+                        header.split(", ").map(|m| m.trim()).collect::<Vec<&str>>();
+                    encodings[0] = encodings[0].split_whitespace().collect::<Vec<&str>>()[1];
+
+                    for encoding in encodings {
+                        if encoding == "gzip" || encoding.contains("gzip") {
+                            return true;
+                        }
+                    }
+                } else if header
+                    .to_lowercase()
+                    .split_whitespace()
+                    .collect::<Vec<&str>>()[1]
+                    == "gzip"
+                {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum HttpCode {
+    Ok,
+    Created,
+    BadRequest,
+    Unauthorized,
+    NotFound,
+    MethodNotAllowed,
+    RequestTimeout,
+    Teapot,
+    InternalServerError,
+}
+
+impl Display for HttpCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HttpCode::Ok => write!(f, "200 OK"),
+            HttpCode::Created => write!(f, "201 Created"),
+            HttpCode::BadRequest => write!(f, "400 Bad Request"),
+            HttpCode::Unauthorized => write!(f, "401 Unauthorized"),
+            HttpCode::NotFound => write!(f, "404 Not Found"),
+            HttpCode::MethodNotAllowed => write!(f, "405 Method Not Allowed"),
+            HttpCode::RequestTimeout => write!(f, "408 Request Timeout"),
+            HttpCode::Teapot => write!(f, "418 I'm a teapot"),
+            HttpCode::InternalServerError => write!(f, "500 Internal Server Error"),
+        }
+    }
+}
+
+/*impl PartialEq for HttpCode {
+    fn eq(&self, other: &Self) -> bool {
+        match self {
+            HttpCode::Ok => match other {
+                HttpCode::Ok => true,
+                _ => false,
+            },
+            HttpCode::Created => match other {
+                HttpCode::Created => true,
+                _ => false,
+            },
+            HttpCode::BadRequest => match other {
+                HttpCode::BadRequest => true,
+                _ => false,
+            },
+            HttpCode::Unauthorized => match other {
+                HttpCode::Unauthorized => true,
+                _ => false,
+            },
+            HttpCode::NotFound => match other {
+                HttpCode::NotFound => true,
+                _ => false,
+            },
+            HttpCode::MethodNotAllowed => match other {
+                HttpCode::MethodNotAllowed => true,
+                _ => false,
+            },
+            HttpCode::RequestTimeout => match other {
+                HttpCode::RequestTimeout => true,
+                _ => false,
+            },
+            HttpCode::Teapot => match other {
+                HttpCode::Teapot => true,
+                _ => false,
+            },
+            HttpCode::InternalServerError => match other {
+                HttpCode::InternalServerError => true,
+                _ => false,
+            },
+        }
+    }
+}*/
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+}
+
+impl HttpMethod {
+    pub fn new(method: &str) -> HttpMethod {
+        if method.to_uppercase().contains("GET") {
+            HttpMethod::GET
+        } else if method.to_uppercase().contains("POST") {
+            HttpMethod::POST
+        } else if method.to_uppercase().contains("PUT") {
+            HttpMethod::PUT
+        } else if method.to_uppercase().contains("DELETE") {
+            HttpMethod::DELETE
+        } else {
+            HttpMethod::PATCH
+        }
+    }
+}
+
+impl Display for HttpMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            HttpMethod::GET => write!(f, "GET"),
+            HttpMethod::POST => write!(f, "POST"),
+            HttpMethod::PUT => write!(f, "PUT"),
+            HttpMethod::PATCH => write!(f, "PATCH"),
+            HttpMethod::DELETE => write!(f, "DELETE"),
+        }
+    }
+}
