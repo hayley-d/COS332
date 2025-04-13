5a034cf79cb15f13897c4532b9a04d46fe81cb64
hayley-d <hayleydod@proton.me>
2025-04-13 13:56:50 +0200

    Starting prac 6

---
+++ 2025-practicals/practical-6/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod api;
+pub mod connection;
+pub mod error;
+pub mod mail;
+pub mod question;
+pub mod request;
+pub mod response;
+pub mod server;
+pub mod socket;
--- 2025-practicals/practical-6/src/main.rs
+++ 2025-practicals/practical-6/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use practical_6::server::set_up_server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = set_up_server().await;
+
+    Ok(())
 }
