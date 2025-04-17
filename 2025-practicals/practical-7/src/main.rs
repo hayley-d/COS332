247d809ce44e2a2af2bba8916e69cecf03902f1e
hayley-d <hayleydod@proton.me>
2025-04-17 19:49:13 +0200

    working

--- 2025-practicals/practical-7/src/main.rs
+++ 2025-practicals/practical-7/src/main.rs
@@ -18,58 +18,119 @@ impl<'a> State<'a> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let state: State = State::new("127.0.0.1", "hayleydod@proton.me", "YskUEuNu-zSRWtiNizzaxg");
+
     let mut stream: tokio::net::TcpStream =
-        match tokio::net::TcpStream::connect(("127.0.0.1", 1143)).await {
+        match tokio::net::TcpStream::connect((state.address, 1143)).await {
             Ok(s) => {
                 println!("Connected to 127.0.0.1 on port 1143");
                 s
             }
             Err(e) => {
                 eprintln!("Failed to connect to SMTP sever: {}", e);
-                return Err(Box::new(e));
+                std::process::exit(1);
             }
         };
+
+    let mut greeting = vec![0u8; 1024];
+    match stream.read(&mut greeting).await {
+        Ok(bytes_read) => {
+            let greeting_str = String::from_utf8_lossy(&greeting[..bytes_read]);
+            println!("Server Greeting: {}", greeting_str);
+        }
+        Err(e) => {
+            eprintln!("Failed to read server greeting: {}", e);
+            std::process::exit(1);
+        }
+    }
+
+    /*match stream.write_all("STARTTLS\r\n".as_bytes()).await {
+        Ok(_) => {
+            println!("STARTTLS command sent");
+        }
+        Err(e) => {
+            eprintln!("Failed to send STARTTLS command: {}", e);
+            std::process::exit(1);
+        }
+    }
+
+    let _ = stream.flush();
 
     let connector: tokio_native_tls::TlsConnector =
         tokio_native_tls::TlsConnector::from(match native_tls::TlsConnector::new() {
             Ok(tls) => tls,
             Err(_) => {
-                eprintln!("Failed to esablish TLS connection");
+                eprintln!("Failed to esablish TLS Connector");
                 std::process::exit(1);
             }
         });
 
-    let mut tls_stream = match connector.connect("127.0.0.1", stream).await {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("Failed to initialize TLS");
-            std::process::exit(1);
-        }
-    };
-
-    let login_command: String = format!(
-        "a001 LOGIN {} {}\r\n",
-        "hayleydod@proton.me", "YskUEuNu-zSRWtiNizzaxg"
+    let connector = tokio_native_tls::TlsConnector::from(
+        native_tls::TlsConnector::builder()
+            .danger_accept_invalid_certs(true)
+            .build()?,
     );
 
-    let _ = tls_stream.write_all(login_command.as_bytes()).await;
+    let mut tls_stream = match connector.connect("localhost", stream).await {
+        Ok(s) => s,
+        Err(e) => {
+            eprintln!("Failed to initialize TLS: {:?}", e);
+            std::process::exit(1);
+        }
+    };*/
+
+    let login_command: String = format!("a001 LOGIN {} {}\r\n", state.username, state.password);
+    let _ = stream.write_all(login_command.as_bytes()).await;
 
     println!("Successful Login");
+    let mut greeting = vec![0u8; 1024];
+    match stream.read(&mut greeting).await {
+        Ok(bytes_read) => {
+            let greeting_str = String::from_utf8_lossy(&greeting[..bytes_read]);
+            println!("Server Login: {}", greeting_str);
+        }
+        Err(e) => {
+            eprintln!("Failed to read server login: {}", e);
+            std::process::exit(1);
+        }
+    }
 
     let select_command: &str = "a002 SELECT inbox\r\n";
-    let _ = tls_stream.write_all(select_command.as_bytes()).await;
+    let _ = stream.write_all(select_command.as_bytes()).await;
     println!("Successful select command");
+    let mut greeting = vec![0u8; 1024];
+    match stream.read(&mut greeting).await {
+        Ok(bytes_read) => {
+            let greeting_str = String::from_utf8_lossy(&greeting[..bytes_read]);
+            println!("Server Select: {}", greeting_str);
+        }
+        Err(e) => {
+            eprintln!("Failed to read server Select: {}", e);
+            std::process::exit(1);
+        }
+    }
 
     let fetch_command: &str = "a003 FETCH 1:* (BODY[HEADER.FIELDS (FROM SUBJECT SIZE)])\r\n";
-    let _ = tls_stream.write_all(fetch_command.as_bytes()).await;
+    stream.write_all(fetch_command.as_bytes()).await?;
     println!("Successful Fetch command");
+    let mut greeting = vec![0u8; 1024];
+    match stream.read(&mut greeting).await {
+        Ok(bytes_read) => {
+            let greeting_str = String::from_utf8_lossy(&greeting[..bytes_read]);
+            println!("Server Fetch: {}", greeting_str);
+        }
+        Err(e) => {
+            eprintln!("Failed to read server Fetch: {}", e);
+            std::process::exit(1);
+        }
+    }
 
     let mut response: Vec<u8> = Vec::new();
-    let _ = tls_stream.read(&mut response).await;
+    stream.read(&mut response).await?;
 
     parse_imap_response(&response);
 
-    Ok(Ok(())?)
+    Ok(())
 }
 
 // Function to parse the IMAP response and extract email headers
