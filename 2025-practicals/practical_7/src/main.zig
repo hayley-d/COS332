2f2dd5ed58116ea40cbc9c9964d42c859284074c
hayley-d <hayleydod@proton.me>
2025-04-23 19:14:15 +0200

    working zig code

--- 2025-practicals/practical_7/src/main.zig
+++ 2025-practicals/practical_7/src/main.zig
@@ -1,30 +1,40 @@
 const std = @import("std");
 const net = std.net;
-const posix = std.posix;
+const print = std.debug.print;
 
 pub fn main() !void {
-    //   const username: [19]u8 = "hayleydod@proton.me";
-    //   const password: [22]u8 = "YskUEuNu-zSRWtiNizzaxg";
+    //const username = "hayleydod@proton.me";
+    //const password = "YskUEuNu-zSRWtiNizzaxg";
+    const port_value = 1143;
 
-    const addr = try std.net.Address.parseIp("127.0.0.1", 1143);
+    const peer = try net.Address.parseIp4("127.0.0.1", port_value);
 
-    const kind: u32 = posix.SOCK.STREAM;
-    const protocol = posix.IPPROTO.TCP;
-    const listener = try posix.socket(addr.any.family, kind, protocol);
-    defer posix.close(listener);
-    //const allocator = std.heap.page_allocator;
-
-    // Connect to ProtonMail Bridge's IMAP server (localhost:1143)
-    //const socket = try net.tcpConnectToHost(allocator, "127.0.0.1", 1143);
+    // Connect to peer
+    const stream = try net.tcpConnectToAddress(peer);
+    defer stream.close();
+    print("Connecting to {}\n", .{peer});
 
     // Login using ProtonMail Bridge's credentials
-    //const login = try std.fmt.format(allocator, "a001 LOGIN {} {}\r\n", .{ username, password });
+    const login_data = "a001 LOGIN hayleydod@proton.me YskUEuNu-zSRWtiNizzaxg\r\n";
+    var writer = stream.writer();
+    var size = try writer.write(login_data);
+    print("Login Command Sending '{s}' to peer, total written: {d} bytes\n", .{ login_data, size });
 
-    // Write the login command to the socket
-    //try socket.writer().writeAll(login);
+    // Select the inbox
+    const select_data = "a002 SELECT inbox\r\n";
+    size = try writer.write(select_data);
+    print("Select Command Sending '{s}' to peer, total written: {d} bytes\n", .{ login_data, size });
 
-    // Fetch message headers
-    //try socket.writer().writeAll("a003 FETCH 1:* (BODY[HEADER.FIELDS (FROM SUBJECT SIZE)])\r\n");
+    // Fetch message Headers
+    const fetch_data = "a003 FETCH 1:* (BODY[HEADER.FIELDS (FROM SUBJECT SIZE)])\r\n";
+    size = try writer.write(fetch_data);
+    print("Fetch Command Sending '{s}' to peer, total written: {d} bytes\n", .{ login_data, size });
+
+    var read_buffer: [1024]u8 = undefined;
+    var reader = stream.reader();
+    const res = try reader.read(&read_buffer);
+    print("Server Response: {d}\n", .{res});
+    print("Server Response: {s}\n", .{read_buffer});
 
     // Read the server's response using the reader
     //const response = try readResponse(&socket, allocator);
@@ -39,10 +49,3 @@ pub fn main() !void {
     // Free the allocated memory for the formatted login string
     //allocator.free(login);
 }
-
-// Function to read the server response from the socket
-//fn readResponse(socket: *net.Stream, allocator: *std.mem.Allocator) ![]const u8 {
-//    var buffer: [1024]u8 = undefined; // Buffer to store response
-//   const bytes_read = try socket.reader().read(buffer[0..]);
-//   return try allocator.alloc(u8, bytes_read); // Allocate memory for the read data
-//}
