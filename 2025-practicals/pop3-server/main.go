5a03374a0bc78c78c54ccaf1711bf617838ff836
hayley-d <hayleydod@proton.me>
2025-04-27 04:39:24 +0200

    Add support for user auth

--- 2025-practicals/pop3-server/main.go
+++ 2025-practicals/pop3-server/main.go
@@ -31,6 +31,14 @@ func handleClient(conn net.Conn) {
 
     buffer := make([]byte,4096)
 
+    // Variables to track state
+    authenticated := false
+    var username string
+
+    users := map[string]string {
+        "hayley@proton.me" : "u21528790",
+    }
+
     for {
         n, err := conn.Read(buffer)
         if err != nil {
@@ -40,6 +48,48 @@ func handleClient(conn net.Conn) {
 
         input := strings.TrimSpace(string(buffer[:n]))
         fmt.Println("Received command:", input)
+
+        parts := strings.SplitN(input, " ",2)
+        command := strings.ToUpper(parts[0])
+        arg := ""
+
+        if len(parts) > 1 {
+            arg = parts[1]
+        }
+
+        switch command {
+        case "USER":
+            username = arg
+            if _, exists := users[username]; exists {
+                conn.Write([]byte("+OK User accepted, please send PASS\r\n"))
+                authenticated = true
+            } else {
+                conn.Write([]byte("-ERR No such user\r\n"))
+            }
+
+        case "PASS":
+            if username == "" {
+                conn.Write([]byte("-ERR USER required first\r\n"))
+            } 
+            expectedPass := users[username]
+            if arg == expectedPass {
+                authenticated = true
+                conn.Write([]byte("+OK Authenticated\r\n"))
+            } else {
+                conn.Write([]byte("-ERR Invalid password\r\n"))
+            }
+
+        case "QUIT":
+            conn.Write([]byte("+OK Goodbye\r\n"))
+            return
+
+        default:
+            if !authenticated {
+                conn.Write([]byte("-ERR Authenticate first\r\n"))
+                continue
+            } 
+            //conn.Write([]byte("-ERR Unknown command\r\n"))
+        }
 
         if strings.HasPrefix(strings.ToUpper(input),"QUIT") {
             conn.Write([]byte("+ OK Goodbye\r\n"))
